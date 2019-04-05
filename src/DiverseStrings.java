7d9fb441daa5b9f83eaed91772b3e0ac2bb6d138
WSP19SCM39H <condor.la@yahoo.com>
2019-04-05 14:25:58 -0500

    Diverse Strings

---
+++ src/DiverseStrings.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class DiverseStrings {
+    //Diverse Strings
+    //都是lower case letters, 放进bucket后判断是否 bucket 的桶是连续的, 且都只出现了一次
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            if (sln(s)) System.out.println("yes");
+            else System.out.println("no");
+        }
+    }
+
+    static boolean sln(String s) {
+        int n = s.length();
+        if (n == 1) return true;
+        int[] bucket = new int[26];
+        for (char c : s.toCharArray()) bucket[c - 'a']++;
+        int left = -1, right = -1, count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (bucket[i] > 1) return false;
+            if (bucket[i] == 1) {
+                if (count == 0) {
+                    left = i;
+                }
+                right = i;
+                count++;
+            }
+        }
+        return count == right - left + 1;
+    }
+}
