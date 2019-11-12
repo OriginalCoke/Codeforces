a3c08ddacd5d3f59eec7d4e0b5650e3c2139de08
OriginalCoke <whuang36@hawk.iit.edu>
2019-11-11 20:17:42 -0600

    test1

---
+++ src/CF550_Div3_D.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CF550_Div3_D {
+    //
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+    }
+
+    public void sln(int[] A) {
+        int max=0,count=0;
+    }
+}
